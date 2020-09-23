95dc54c8594cd889a3e991b0e4bca32cb92b73d6
besi <besalelu.php@gmail.com>
2020-09-24 00:59:07 +0530

    node js crash course

---
+++ index.js
@@ -0,0 +1,6 @@
+const Person = require('./person');
+
+const person1 = new Person('John Doe', 31);
+const person2 = new Person('Will Smith', 42);
+person1.greeting();
+person2.greeting();
---
+++ readme.txt
@@ -0,0 +1,4 @@
+1. javascript runtime
+2. built on the V8 javascript engine - same as google chrome
+3. written in C++
+4. javascript code on the server
